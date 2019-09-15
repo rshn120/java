192700ef2be53988fd4197475684909a47d8493a
상현 <@DESKTOP-9GC0H79>
2019-09-15 12:36:32 +0900

    4

--- HelloWorld/src/hello/Exercise05.java
+++ HelloWorld/src/hello/Exercise05.java
@@ -3,6 +3,8 @@ package hello;
 public class Exercise05 {
 public static void main(String[] args) {
 	int value=356;
-	System.out.println(value-=value % 100);
+	//System.out.println(value - 56);
+	System.out.println(value -= value % 100);
+	
 }
 }

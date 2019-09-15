18fe2871d58353d2517d086da78433f0a439efe8
상현 <@DESKTOP-9GC0H79>
2019-09-15 17:55:18 +0900

    4

--- HelloWorld/src/hello/Exercise407.java
+++ HelloWorld/src/hello/Exercise407.java
@@ -20,9 +20,11 @@ public static void main(String[] args) {
 		if(cb == 1) { //입력 받은 숫자가 1인지 비교 맞으면 실행
 			System.out.print("예금액> ");
 			balance += scanner.nextInt(); //balance 값 0에서 입력받는 숫자를 더해서 balance에 넣는다.
+			System.out.println("현재 금액> "+balance);
 		} else if(cb == 2) { //입력받는 숫자가 2인지 비교 맞으면 실행
 			System.out.print("출급액> ");
 			balance -= scanner.nextInt(); //위에서 입력 받은 숫자가 들어간 balance값에 입력 받는 숫자값을 빼서 balance에 저장
+			System.out.println("현재 금액> "+balance);
 		} else if(cb == 3) { //입력받는 숫자가 3인지 비교 맞으면 실행
 			System.out.println("잔고> "+balance);// balance에 남아있는 값을 출력
 		} else if(cb == 4) {// 입력받은 값이 4이면 false를 만나 종료한다.

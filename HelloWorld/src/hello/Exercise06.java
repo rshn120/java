f5ee0d5a1f1ecc0f103a7e29a6330721062b92fd
상현 <@DESKTOP-9GC0H79>
2019-09-15 13:06:35 +0900

    4

--- HelloWorld/src/hello/Exercise06.java
+++ HelloWorld/src/hello/Exercise06.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	int lengthTop = 5;
 	int lengthBottom = 10;
 	int height = 7;
-	double area = (7*(lengthTop+lengthBottom)/2);
+	double area = (((lengthTop+lengthBottom)*7)/2);
 	System.out.println(area);
 }
 }
--- HelloWorld/src/hello/Exercise07.java
+++ HelloWorld/src/hello/Exercise07.java
@@ -4,8 +4,17 @@ public class Exercise07 {
 public static void main(String[] args) {
 	int x=10;
 	int y=5;
-	
 	System.out.println( (x>7) && (y<=5));
+	/* x(10) > 7 = true 
+	 *  && (and 연산) 두값이 모두 true일때 true
+	 *  y(5) <= 5 y값이 5보다 크거나 같다 true
+	 *  출력값 true
+	 */ 
 	System.out.println((x%3 ==2) || (y%2 !=1));
+	/* x(10) % 3 나머지는 1 == 2   1과 2는 같지 않으니 false 
+	 * || (or 연산) 둘중에 하나만 같을때 true
+	 * y(5) %2 = 1  !=( 좌우 값이 다른지 비교.  같으니 결고는 false
+	 * 좌변 값과 우변 값이 같아서 or 연산으로 false가 된다.
+	 */
 }
 }

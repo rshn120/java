89de2b9e6eac4f199ec5d5ccefe382e4e6f82455
상현 <@DESKTOP-9GC0H79>
2019-09-15 17:12:55 +0900

    test

--- HelloWorld/src/hello/Exercise407.java
+++ HelloWorld/src/hello/Exercise407.java
@@ -13,9 +13,10 @@ public static void main(String[] args) {
 		System.out.println("-------------------------------");
 		System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
 		System.out.println("-------------------------------");
-		System.out.println("선택> ");
+		System.out.print("선택> ");
 		
 		balance = scanner.nextInt();
+		
 		if(balance == 1) {
 			System.out.println("예금액> 10000");
 		} else if(balance == 2) {
--- HelloWorld/src/hello/For1.java
+++ HelloWorld/src/hello/For1.java
@@ -2,7 +2,8 @@ package hello;
 
 public class For1 {
 	public static void main(String[] args) {
-	for (int i=1; i <= 100; i++) {
+	int sum=0;
+		for (int i=1; i <= 100; i++) {
 		sum += 1;
 	
 		System.out.println("1~100 sum :"+ sum);

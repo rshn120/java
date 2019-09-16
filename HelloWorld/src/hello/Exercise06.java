b3b021b06b3a36dbe7f6875d6f1b8c8e4d2749b7
User <User@YD03-19>
2019-09-16 17:48:57 +0900

    debug

--- HelloWorld/src/hello/Exercise06.java
+++ HelloWorld/src/hello/Exercise06.java
@@ -4,7 +4,7 @@ public class Exercise06 {
 public static void main(String[] args) {
 	int lengthTop = 5;
 	int lengthBottom = 10;
-	int height = 7;
+//	int height = 7;
 	double area = (((lengthTop+lengthBottom)*7)/2);
 	System.out.println(area);
 }
--- HelloWorld/src/hello/Hello.java
+++ HelloWorld/src/hello/Hello.java
@@ -2,7 +2,7 @@ package hello;
 
 public class Hello {
 	public static void main(String[] args) {
-		int num1;
+	//	int num1;
 		System.out.println("Hello, World");
 	}
 

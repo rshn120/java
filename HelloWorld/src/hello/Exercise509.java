bc06ed8465aa0d144a9432d0cc5e75d05b9aab2f
User <User@DESKTOP-336P9P4>
2019-09-17 20:22:46 +0900

    509

--- HelloWorld/src/hello/Exercise509.java
+++ HelloWorld/src/hello/Exercise509.java
@@ -21,18 +21,34 @@ public class Exercise509 {
 				stdNum = sc.nextInt();
 				scr = new int[stdNum];
 				System.out.println("학생수> " + stdNum);
-			} else if(sNo == 2) {
-				//scr  점수 작성부분
+			} else if(sNo == 2) { //scr  점수
+				for(int i=0;i<stdNum;i++) {
+					System.out.println("scores["+i+"]> ");
+					int scr=sc.nextInt();
+					scr[i]=scr;
+				}
 				
-			} else if(sNo == 3) {
-				//3
-			} else if(sNo == 4) {
-				//4
-			} else if(sNo == 5) {
+			} else if(sNo == 3) {	//3 리스트
+				for(int i=0;i<stdNum;i++) {
+					System.out.println("scores["+i+"]:"+scores[i]);
+				}
+			} else if(sNo == 4) {	//4 분석
+				int max=0, sum=0, avg=0;
+				for(int i=0;i<stdNum;i++) {
+					sum += scr[i];
+					for(int j=0; j<scr[i];j++) {
+						max=scr[i];
+					}
+					avg=sum/stdNum;
+					System.out.println("최고 점수: "+max);
+					System.out.println("평균 점수: "+avg);
+				}
+			} else if(sNo == 5) {   //종료
 				run = false;
+				System.out.println("프로그램 종료");
 			}
 		}
-		System.out.println("프로그램 종료");
+		
 		sc.close();
 	}
 

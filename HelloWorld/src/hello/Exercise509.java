9d39a4f9e090ae690859e2e1b894ecbea120de22
User <User@DESKTOP-336P9P4>
2019-09-17 21:25:17 +0900

    last 09

--- HelloWorld/src/hello/Exercise509.java
+++ HelloWorld/src/hello/Exercise509.java
@@ -1,39 +1,57 @@
 package hello;
-
 import java.util.Scanner;
-
 public class Exercise509 {
 	public static void main(String[] args) {
 		boolean run = true;
-		int stdNum = 0;  //학생수
-		int[] scr = null; // 점수
-		Scanner sc = new Scanner(System.in);
+		int stdn = 0;  //학생수, 책에는 studentNum
+		int[] scr = null; // 점수, 책에는 scores
+		Scanner sc = new Scanner(System.in); //책에는 scanner
 		
-		while(run) {
-			System.out.println("---------------------------------------------");
+		while(run) { //조건이 참이라 실행 ~
+			System.out.println("------------------------------------------");
 			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
-			System.out.println("---------------------------------------------");
+			System.out.println("------------------------------------------");
 			System.out.print("선택> ");
 			
-			int sNo = sc.nextInt();
+			int selectNo = sc.nextInt(); 
 			
-			if(sNo == 1) {
-				stdNum = sc.nextInt();
-				scr = new int[stdNum];
-				System.out.println("학생수> " + stdNum);
-			} else if(sNo == 2) {
-				//scr  점수 작성부분
-				
-			} else if(sNo == 3) {
-				//3
-			} else if(sNo == 4) {
-				//4
-			} else if(sNo == 5) {
+			if(selectNo == 1) {
+				System.out.println("학생수> ");
+				stdn = sc.nextInt(); //학생수 : 배열 갯수를 입력 받는다.
+				scr = new int[stdn]; //학생수 배열을 scores에 넣는다.
+			}		
+			else if(selectNo == 2) {	// 점수
+				for(int i=0;i<stdn;i++) {//i가 학생수 보다 많을때까지 돌아라
+					System.out.println("scores["+i+"]> ");
+					int a=sc.nextInt(); //점수를 입력받기 위해 int a 선언
+					scr[i]=a; //a로 받은 값을 scores 배열로 넣어줌
+				}				
+			} 
+			else if(selectNo == 3) {	//3 리스트
+				for(int i=0; i<stdn; i++) { //i가 학생수 보다 많을때까지 돌아라
+					System.out.println("scores["+i+"]:"+scr[i]);
+				}
+			} 
+			
+			else if(selectNo == 4) {	//4 평균
+				int max=0, sum=0;
+				double avg=0; // 평균은 소수점이라 int로 하면 안된다.
+				for(int i=0; i<stdn; i++) {//i가 학생수 보다 많을때까지 돌아라
+					sum += scr[i]; // 입력받은 점수를 더해서 sum에 넣어라
+					if(scr[i]>max) //입력 받은 점수가 max값보다 클때 까지 돌아~
+						max = scr[i]; //가장 큰 점수를 맥스에 넣어라 표현하기가 애메하네
+				}
+				avg=(double)sum/stdn; //총합 나누기 학생수 소수점때문에int sum을 double로 변환 이거 안하면 오류
+				System.out.println("최고 점수: "+max);
+				System.out.println("평균 점수: "+avg);
+			} 
+			
+			else if(selectNo == 5) {   //5 종료
 				run = false;
-			}
+				System.out.println("프로그램 종료");			
+			}	
+			}sc.close();
 		}
-		System.out.println("프로그램 종료");
-		sc.close();
 	}
 
-}
+

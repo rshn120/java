aede4fe02ae1239302e518aa334cf2e050c7e570
User <User@YD03-19>
2019-09-24 17:50:33 +0900

    a

--- HelloWorld/src/hello/MemberExample2.java
+++ HelloWorld/src/hello/MemberExample2.java
@@ -3,20 +3,21 @@ package hello;
 import java.util.Scanner;
 
 public class MemberExample2 {
-	private static Member[] accArray = new Member[50];
+	private static Member[] memberArray = new Member[50];
 	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		boolean run = true;
+		int menu=0;
 		while(run) {
 			System.out.println("1.id생성 2.전체리스트 3.id조회 4.종료");
-			int menu = sc.nextInt();sc.nextLine();
+			menu = sc.nextInt();sc.nextLine();
 			if(menu==1) {
-				createAccount();
+				createMember();
 			}else if (menu==2) {
-				getAccount();
+				getMember();
 			}else if (menu==3) {
-				deposit();
+				getMem();
 			}else if (menu==4) {
 				run = false;
 			}
@@ -24,52 +25,83 @@ public class MemberExample2 {
 	}
 
 	//멤버 인스턴스 만드는 메소드.
-	public static void createAccount() {
-		System.out.println("생성 id를 입력: ");
-		String accId = sc.nextLine();
-		System.out.println("생성 사용자명 입력: ");
-		String accName = sc.nextLine();
-		System.out.println("생성 비밀번호 입력: ");
-		String accPswd = sc.nextLine();
-		System.out.println("생성 나이: ");
-		int accAge = sc.nextInt();
+	public static void createMember() {
+		System.out.println("id를 입력: ");
+		String Id = sc.nextLine();
+		System.out.println("사용자명 입력: ");
+		String Name = sc.nextLine();
+		System.out.println("비밀번호 입력: ");
+		String Pswd = sc.nextLine();
+		System.out.println("나이: ");
+		int Age = sc.nextInt();
 		
-		Member accnt = new Member(accId, accName, accPswd, accAge);
-		for(int i=0;i<accArray.length;i++) {
-			if(accArray[i]==null) {
-				accArray[i] = accnt;
+		Member accnt = new Member(Id, Name, Pswd, Age);
+		for(int i=0;i<memberArray.length;i++) {
+			if(memberArray[i]==null) {
+				memberArray[i] = accnt;
 				break;
 			}
 		}		
 	}
 	//멤버리스트 조회하는 메소드.
-	public static void getAccount() {
+	public static void getMember() {
 		System.out.println("getAccount");
-		for(Member a : accArray) {
+		for(Member a : memberArray) {
 			if(a != null)//null 값이 없을 경우에 실행하라
 			System.out.println("아이디 : "+a.getId()+", 사용자: "+a.getName()+", 비밀번호: "+a.getPassword()+", 나이: "+a.getAge());
 		}
 	}
 	//멤버 id 조회
-	public static void deposit() {
-//		System.out.println("ID 조회");
+	public static void getMem() {
 		System.out.println("조회할 ID를 입력: ");
-		String accId = sc.next();
+		String Id = sc.nextLine();
 		System.out.println("조회할 pw를 입력: ");
-		String accPswd = sc.next();
-		for(Member a : accArray) {
-			if(a!= null && a.getId().equals(accId) && a.getPassword().equals(accPswd)) {
-				
-				System.out.println("사용자: "+a.getName()+", 나이: "+a.getAge());
-				break;
-			}else if(a!=null && a.getId().equals(accId)) {
-				System.out.println("패스워드가 틀렸습니다.");
-				break;
-			}else if(a!=null && a.getPassword().equals(accPswd)) {
-				System.out.println("ID가 틀렸습니다.");
-				break;
+		String Pswd = sc.nextLine();
+		boolean isId = false, isPw=false;
+		String name = null;
+		int age=0;
+		for(Member a : memberArray) {
+			if(a !=null&&a.getId().equals(Id)) {
+				if(a.getPassword().equals(Pswd)) {
+					isId = true;
+					isPw = true;
+					name = a.getName();
+					age = a.getAge();
+					break;
+				}else { //id 맞고 pw 틀리다
+					isId=true;
+					isPw=false;
+					break;
+				}
+			}else { // id, pw 다 틀리다
+				isId = false;
+				isPw = false;
 			}
-				
+			
+//			if(a!= null && a.getId().equals(Id) && a.getPassword().equals(Pswd)) {
+//				//id랑 pw 같으면 출력
+//				System.out.println("사용자: "+a.getName()+", 나이: "+a.getAge());
+//				break;
+//			}else if(a!=null && a.getId().equals(Id)) {//pw만 다를 경우 
+//				System.out.println("패스워드가 틀렸습니다.");
+//				break;
+//			}else if(a!=null && a.getPassword().equals(Pswd)) {//id가 다를경우
+//				System.out.println("ID가 틀렸습니다.");
+//				break;
+//			}else if(a!=null && a.getPassword().equals(Pswd)) {//id, pw가 다를경우
+//				System.out.println("ID가 틀렸습니다.");
+//				break;
+//			}
+		}
+		if(isId==true && isPw == true) {
+			System.out.println("이름은 : "+name+", 나이 : "+age); 
+						
+		}else if(isId==true && isPw==false) {
+			System.out.println("패스워드가 틀립니다.");
+			
+		}else if(isId==false && isPw==false) {
+			System.out.println("존재하지 않는 아이디입니다.");
+			
 		}
 	}
 	public static Member findAccount(String ano) {

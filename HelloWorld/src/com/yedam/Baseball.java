69d253d745f7570519c6281de1db710e3003c51c
User <User@YD03-19>
2019-09-26 18:04:32 +0900

    a

--- HelloWorld/src/com/yedam/Baseball.java
+++ HelloWorld/src/com/yedam/Baseball.java
@@ -8,32 +8,44 @@ public class Baseball {
 		Scanner sc = new Scanner(System.in);
 
 		int[] in = new int[3];
-		int st = 0;
-		int ba = 0;
+		
 		boolean run = true;
-		int ran[] = {(int) (Math.random() * 9) + 1,(int) (Math.random() * 9) + 1,(int) (Math.random() * 9) + 1};
+		int ran[] = {((int) (Math.random() * 9) + 1)
+				, ((int) (Math.random() * 9) + 1) ,
+				((int) (Math.random() * 9) + 1)};
 		
 		for(int r:ran)
 			System.out.print(r+" ");
 
 		System.out.println();
 		
-		while (run) {
-			for (int i = 0; i < ran.length; i++) {
-				ran[i] = (int) (Math.random() * 9) + 1;
-			}
-			if (ran[0] != ran[1] && ran[0] != ran[2] && ran[1] != ran[2]) {
-				run = false;
-			}
-			
-		}
+//		while (run) {
+//			for (int i = 0; i < ran.length; i++) {
+//				ran[i] = (int) (Math.random() * 9) + 1;
+//			}
+//			if (ran[0] != ran[1] && ran[0] != ran[2] && ran[1] != ran[2]) {
+//				run = false;
+//			}
+//			
+//		}
+		
+		for(int r:ran)
+			System.out.print(r+" ");
 		run = true;
 		
+		int st = 0;
+		int ba = 0;
+		
 		while (run) {
+			st = 0;
+			ba = 0;
+			
 			System.out.println("1: ");
 			in[0] = sc.nextInt();
+			
 			System.out.println("2: ");
 			in[1] = sc.nextInt();	
+			
 			System.out.println("3: ");
 			in[2] = sc.nextInt();	
 			
@@ -53,7 +65,9 @@ public class Baseball {
 			}
 			System.out.println("======================");
 		}
-		run = false;
+		
+		if(st==3)
+			run = false;
 
 	}
 }

5a5e1a43f4bef1cc171ce7134bb7ed29a7cc9945
상현 <@DESKTOP-9GC0H79>
2019-10-02 18:49:50 +0900

    a

---
+++ HelloWorld/src/suna/AccountExample.java
@@ -0,0 +1,11 @@
+package suna;
+
+public class AccountExample {
+	public static void main(String[] args) {
+	Account account = new Account();
+	
+	account.deposit(10000);
+	System.out.println("예금액: "+account.);
+	
+}
+}
---
+++ HelloWorld/src/suna/BaException.java
@@ -0,0 +1,8 @@
+package suna;
+
+public class BaException extends Exception {
+	public BaException() {}
+	public BaException(String message) {
+		super(message);
+	}
+}
